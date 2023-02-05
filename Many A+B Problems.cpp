c227b5febbb1dd665bb7d77709c298b894fe1566
ABHINAV ANAND <95186533+abhinav8925@users.noreply.github.com>
2023-02-05 18:27:14 +0530

    Create Many A+B Problems.cpp

---
+++ Many A+B Problems.cpp
@@ -0,0 +1,29 @@
+/*
+
+    JAI SHREE KRISHNA
+
+    Date -> 4th February 2023
+    Code By -> Abhinav Anand
+    
+    Problem Link -> https://atcoder.jp/contests/abc288/tasks/abc288_a
+
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+
+int main()
+{
+  int n;
+  cin>>n;
+  
+  while(n--)
+  {
+    ll a,b;
+    cin>>a>>b;
+    cout << a+b << endl;
+  }
+ }
+    
